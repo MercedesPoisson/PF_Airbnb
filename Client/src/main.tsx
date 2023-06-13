90ba237dacc888a4794c43a6a2f9e3b096a28698
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-12 23:21:43 -0300

    Form y algunos sub componentes

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom";
-import App from "./App.tsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store/store.ts";
+import React from 'react'
+import { BrowserRouter } from "react-router-dom"
+import ReactDOM from 'react-dom';
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // import { Provider } from "react-redux";
