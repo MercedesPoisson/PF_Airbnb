b857f0760b1cf98420babef49ceb8d070698cc0c
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-13 21:01:56 -0300

    actualizaciones actions, reducer y types

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,15 +3,15 @@ import { BrowserRouter } from "react-router-dom"
 import ReactDOM from 'react-dom';
 import App from './App.tsx'
 import './index.css'
+// import store from './redux/store.tsx';
+// import { Provider } from "react-redux"
 
 ReactDOM.render(
-  <BrowserRouter>
+    <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>,
+    
   document.getElementById('root')
 );
-
-// import { Provider } from "react-redux";
-// import store from '../src/redux/store';
