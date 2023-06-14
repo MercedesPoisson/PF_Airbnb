2a701bf699a8733e65ffc0955f4d5a991852e879
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-13 22:04:21 -0300

    cambios form, pasaje de data primer componente

--- Client/src/views/Form/Location.tsx
+++ Client/src/views/Form/Location.tsx
@@ -17,20 +17,26 @@ function Location() {
             <div className="sticky top-0">
             <NavBar />
         </div>
-            <div className="grid grid-cols-2 font-cairo gap-2 w-3/4 mx-auto h-96 ">
+            <div className="grid grid-cols-2 font-cairo gap-2 w-3/4 mx-auto ">
         <div>
           <div className="font-cairo text-2xl ">¿Dónde queda tu propiedad?</div>
           <p className="font-cairo">Solo vamos a compartir tu dirección exacta una vez confirmada la reserva.</p>  
         </div>
-        <div className="h-[470px]">
-          <iframe
-            src="https://storage.googleapis.com/maps-solutions-cpse4u0jxa/address-selection/brph/address-selection.html"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            loading="lazy"
-          ></iframe>
-        </div>
+        <div className="items-center">
+         <div className="relative">
+           <input className="pl-8 w-96 h-10 border rounded-md mb-2" type="text" placeholder="Ingrese la dirección" />
+           <i className="fa fa-location-dot absolute left-2 top-3 text-gray-600"></i>
+         </div>
+         <div className="relative">
+           <input className="pl-8 w-96 h-10 border rounded-md mb-2" type="text" placeholder="Ingrese la localidad" />
+           <i className="fa fa-location-dot absolute left-2 top-3 text-gray-600"></i>
+         </div>
+         <div className="relative ">
+           <input className="pl-8 w-96 h-10 border rounded-md " type="text" placeholder="Ingrese la provincia" />
+           <i className="fa fa-location-dot absolute left-2 top-3 text-gray-600"></i>
+         </div>         
+       </div>
+        
         <div className="col-span-2 font-cairo-play flex justify-start ml-10">
         <button className="border border-argentina rounded p-1 w-32 mt-4 mr-2" onClick={handlePrevious}>Anterior</button>
           <button className="border border-argentina rounded p-1 w-32 mt-4" onClick={handleNext}>Siguiente</button>
@@ -44,6 +50,16 @@ function Location() {
   export default Location;
   
     //AIzaSyB-QLb5dW-MDp_UPw53W7OuQ9mHSZZPBq8
+
+    // <div className="h-[470px]">
+    //       <iframe
+    //         src="https://storage.googleapis.com/maps-solutions-cpse4u0jxa/address-selection/brph/address-selection.html"
+    //         width="100%"
+    //         height="100%"
+    //         style={{ border: 0 }}
+    //         loading="lazy"
+    //       ></iframe>
+    //     </div>
   
 
 // function Location() {
--- Client/src/views/Form/PropertyType.tsx
+++ Client/src/views/Form/PropertyType.tsx
@@ -1,43 +1,62 @@
 import { useNavigate } from "react-router-dom";
 import NavBar from "./navBar";
+import { useState } from "react";
 
 function PropertyType() {
     const navigate = useNavigate();
+    const [propertyType, setPropertyType] = useState("");
 
     const handlePrevious = () => {
         navigate(-1);
     }
 
     const handleNext = () => {
-        navigate("/post/location");
+      if (propertyType !== '') { //verifico si propertyType no esta vacio
+        navigate(`/post/location?type=${propertyType}`); //paso los datos como parametros en la url y los recupero en el siguiente componente
+      }
     }
 
-
+    const handlePropertyTypeSelect = (type: string) => {
+      setPropertyType(type);
+    }
 
     return (
+      <div>
+      <div className="sticky top-0">
+        <NavBar />
+      </div>
+      <div className="grid grid-cols-2 font-cairo gap-2 w-3/4 mx-auto">
         <div>
-            <div className="sticky top-0">
-            <NavBar />
+          <div className="text-2xl">Paso 1</div>
+          <div className="text-2xl">Contanos acerca de tu propiedad</div>
+          <p>
+            En este paso, te vamos a preguntar qué tipo de propiedad tenés. <br /> Cuál es la ubicación y cuántos
+            huéspedes pueden quedarse en el alojamiento.
+          </p>
+          <div className="text-2xl mt-4">¿Cuál de estas opciones describe mejor tu propiedad?</div>
         </div>
-           <div className="grid grid-cols-2 font-cairo gap-2 w-3/4 mx-auto">
-        <div>
-           <div className=" text-2xl">Paso 1</div>
-        <div className="text-2xl">Contanos acerca de tu propiedad</div>
-        <p>En este paso, te vamos a preguntar qué tipo de propiedad tenés. <br/> Cuál es la ubicación y cuántos huéspedes pueden quedarse en el alojamiento.</p>
-        <div className="text-2xl mt-4">¿Cuál de estas opciones describe mejor tu propiedad?</div> 
-        </div>
-        
-        <div className=" font-cairo mt-3">
-          <div className=" mb-3">
-            <button className="flex flex-col items-center justify-center bg-transparent border border-black rounded-md focus:outline-none w-28">
+
+        <div className="font-cairo mt-3">
+          <div className="mb-3">
+            <button
+              className={`flex flex-col items-center justify-center bg-transparent border rounded-md focus:outline-none w-28 ${
+                propertyType === 'Casa' ? 'border-argentina' : 'border-black'
+              }`}
+              onClick={() => handlePropertyTypeSelect('Casa')}
+            >
               <span className="mb-2">
                 <i className="fa-solid fa-house text-gray-600"></i>
               </span>
               <span className="text-center">Casa</span>
             </button>
           </div>
-          <div className=" mb-3">
-            <button className="flex flex-col items-center justify-center bg-transparent border border-black rounded-md focus:outline-none w-28">
+          <div className="mb-3">
+            <button
+              className={`flex flex-col items-center justify-center bg-transparent border rounded-md focus:outline-none w-28 ${
+                propertyType === 'Departamento' ? 'border-argentina' : 'border-black'
+              }`}
+              onClick={() => handlePropertyTypeSelect('Departamento')}
+            >
               <span className="mb-2">
                 <i className="fa-solid fa-building text-gray-600"></i>
               </span>
@@ -45,7 +64,12 @@ function PropertyType() {
             </button>
           </div>
           <div className="col-span-1">
-            <button className="flex flex-col items-center justify-center bg-transparent border border-black rounded-md focus:outline-none w-28">
+            <button
+              className={`flex flex-col items-center justify-center bg-transparent border rounded-md focus:outline-none w-28 ${
+                propertyType === 'Habitación' ? 'border-argentina' : 'border-black'
+              }`}
+              onClick={() => handlePropertyTypeSelect('Habitación')}
+            >
               <span className="mb-2">
                 <i className="fa-solid fa-bed text-gray-600"></i>
               </span>
@@ -54,13 +78,16 @@ function PropertyType() {
           </div>
         </div>
         <div className="col-span-2 font-cairo-play flex justify-start ml-10">
-        <button className="border border-argentina rounded p-1 w-32 mt-4 mr-2" onClick={handlePrevious}>Anterior</button>
-        <button className="border border-argentina rounded p-1 w-32 mt-4" onClick={handleNext}>Siguiente</button>
-      </div>
-      </div> 
+          <button className="border border-argentina rounded p-1 w-32 mt-4 mr-2" onClick={handlePrevious}>
+            Anterior
+          </button>
+          <button className="border border-argentina rounded p-1 w-32 mt-4" onClick={handleNext} disabled={propertyType === ''}>
+            Siguiente
+          </button>
         </div>
-      
-    );
-  }
+      </div>
+    </div>
+  );
+}
   
-  export default PropertyType;
+  export default PropertyType
