9c374f291fb1ed3a0497ad823910550686140fc8
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-17 16:35:34 -0300

    Form + mascota + preview condicional

--- Client/src/views/Form/Form/Form.tsx
+++ Client/src/views/Form/Form/Form.tsx
@@ -13,6 +13,7 @@ import postServices from "../../../redux/actions/postProperties";
 import { useDispatch } from "react-redux";
 import Cloudinary from "./Cloudinary";
 import AvailableDates from "./AvailableDates";
+import Pets from "./Pets";
 
 interface FormData {
   [x: string]: any;
@@ -243,6 +244,15 @@ const Form = () => {
         )}
 
         {formData.step === 10 && (
+          <Pets
+            previousStep={previousStep}
+            nextStep={nextStep}
+            formData={formData}
+            setFormData={setFormData}
+          />
+        )}
+
+        {formData.step === 11 && (
           <AvailableDates
           previousStep={previousStep}
           nextStep={nextStep}
@@ -251,7 +261,7 @@ const Form = () => {
           
         )}
 
-        {formData.step === 11 && (
+        {formData.step === 12 && (
           <Preview
           previousStep={previousStep}
           handlePost={handlePost}
--- Client/src/views/Form/Form/Preview.tsx
+++ Client/src/views/Form/Form/Preview.tsx
@@ -1,36 +1,37 @@
-// import postServices from "../../../redux/actions/postProperties";
-
 const Preview = (props) => {
-  
-    return (
-        <div>
-            <div className="grid grid-cols-1 font-cairo gap-2 w-3/4 mx-auto">
+  return (
+    <div>
+      <div className="grid grid-cols-1 font-cairo gap-2 w-3/4 mx-auto">
         <div>
           <div className="font-cairo text-2xl">Revisá tu anuncio</div>
           <p className="font-cairo">Esta es la información que van a ver tus huéspedes</p>
         </div>
         <div>
-            <h1 className="font-bold font-cairo-play ">{props.formData.title}</h1>
-            <h5>{props.formData.location}, {props.formData.province}</h5>
-            <h3>{props.formData.description}</h3>
-            <h3>Tipo de Propiedad: {props.formData.property_type}</h3>
-            <h2>Cantidad de Huespedes: {props.formData.max_guests}</h2>
-            <h2>Cantidad Dormitorios: {props.formData.rooms_number}, Catidad de Camas: {props.formData.beds_number}, cantidad de Baños: {props.formData.bathrooms_number}</h2>
-            <h4 className="font-bold">Precio por Noche: ${props.formData.price_per_night}</h4>
-            <h3>Servicios: {props.formData.services.join(", ")}</h3>
-            <h3>Esta propiedad se encuentra disponible desde: {props.start_date} hasta {props.end_date}</h3>
-            {props.formData.weekly_discount || props.formData.monthly_discount ? (
-              <h4 className="font-bold font-cairo-play">
-                Esta propiedad ofrece descuentos! {props.formData.weekly_discount}, {props.formData.monthly_discount}
-              </h4>
-            ) : null}
+          <h1 className="font-bold font-cairo-play ">{props.formData.title}</h1>
+          <h5>{props.formData.location}, {props.formData.province}</h5>
+          <h3>{props.formData.description}</h3>
+          <h3>Tipo de Propiedad: {props.formData.property_type}</h3>
+          <h2>Cantidad de Huespedes: {props.formData.max_guests}</h2>
+          <h2>Cantidad Dormitorios: {props.formData.rooms_number}, Catidad de Camas: {props.formData.beds_number}, cantidad de Baños: {props.formData.bathrooms_number}</h2>
+          <h4 className="font-bold">Precio por Noche: ${props.formData.price_per_night}</h4>
+          <h3>Servicios: {props.formData.services.join(", ")}</h3>
+          {props.formData.acceptsPets && (
+            <h3 className="font-semibold">
+              Podes venir a disfrutar con tu mascota <i className="fa-solid fa-paw ml-2 text-argentina"></i>
+            </h3>
+          )}
+          <h3>Esta propiedad se encuentra disponible desde: {props.start_date} hasta {props.end_date}</h3>
+          {props.formData.weekly_discount || props.formData.monthly_discount ? (
+            <h4 className="font-bold font-cairo-play">
+              Esta propiedad ofrece descuentos! {props.formData.weekly_discount}, {props.formData.monthly_discount}
+            </h4>
+          ) : null}
         </div>
         <div className="flex space-x-4">
           {props.formData.images.map((image, index) => (
             <img className="h-60" key={index} src={URL.createObjectURL(image)} alt={`Image ${index}`} />
           ))}
         </div>
-        
         <div className="col-span-1 font-cairo-play flex items-center justify-start mr-10">
           <button className="border border-argentina rounded p-1 w-32 mt-4 mr-2" onClick={props.previousStep}>
             Anterior
@@ -39,6 +40,7 @@ const Preview = (props) => {
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
+
 export default Preview;
