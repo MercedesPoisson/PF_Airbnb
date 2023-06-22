6f59c3a55e70c7b218c20d0d0fe6ed3f74df4a79
augustoorsi <augusford@hotmail.com>
2023-06-22 16:28:44 -0300

    actions realizadas

---
+++ Client/src/redux/actions/updatePropertStatus.tsx
@@ -0,0 +1,15 @@
+import { Dispatch, AnyAction } from "redux";
+import { PUT_PROPERTY_STATUS } from "./actionsType";
+import axios from "axios";
+
+const updateProperty = (property: any) => {
+    return async (dispatch:Dispatch<AnyAction>)=>{
+        const {id_property}= property
+        try {
+            await axios.put(`http://localhost:3001/property/${id_property}/deactivate`)
+            dispatch({type: PUT_PROPERTY_STATUS})
+        } catch (error) {
+            console.log("Error al actualizar la propiedad:", error)
+        }
+    }
+}
