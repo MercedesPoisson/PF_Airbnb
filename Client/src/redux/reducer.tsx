3ad0c26fb9ed42cf2fe40cbd89e95069ae83c110
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-25 19:03:29 -0300

    Ruta action reducer get All users lista

--- Client/src/redux/reducer.tsx
+++ Client/src/redux/reducer.tsx
@@ -1,5 +1,5 @@
 import { Reducer } from "redux";
-import { GET_USER, CREATE_USER, GET_PROPERTIES, GET_PROPERTY_DETAIL, GET_SERVICES, POST_PROPERTIES, GET_PROVINCES, PUT_USER, PUT_PROPERTY, PUT_PROPERTY_STATUS } from "./actions/actionsType";
+import { GET_ALL_USERS, GET_USER, CREATE_USER, GET_PROPERTIES, GET_PROPERTY_DETAIL, GET_SERVICES, POST_PROPERTIES, GET_PROVINCES, PUT_USER, PUT_PROPERTY, PUT_PROPERTY_STATUS } from "./actions/actionsType";
 import { State, Action } from "./Types"
 
 
@@ -35,6 +35,8 @@ const rootReducer: Reducer<State, Action> = (state = inictialState, { type, payl
             return {...state}
         case PUT_PROPERTY_STATUS:
             return {...state}
+        case GET_ALL_USERS:
+            return {...state, properties: payload}
         default:
             return state;
     }
