c10e51365e2eba14df8ad0e01299b3e549a713e0
augustoorsi <augusford@hotmail.com>
2023-06-25 17:39:37 -0300

    favoritos

--- Client/src/views/UserProfile/favoritos.tsx
+++ Client/src/views/UserProfile/favoritos.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import UserNavBar from "./UserNavBar";
+import { useDispatch } from "react-redux";
 
 const Favoritos = () => {
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getFavor)
+    })
     return(
         <div>
             <UserNavBar />
-             soy favoritos
         </div>
     )
 }
