8d5a0f716af5cb055aa85346113a7894954d5d87
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-26 15:25:15 -0300

    cambios en panel de usuarios

--- Client/src/views/UserProfile/Account.tsx
+++ Client/src/views/UserProfile/Account.tsx
@@ -27,7 +27,101 @@ const Account = () => {
   return (
     <div>
       <UserNavBar />
-      <div className="grid grid-cols-3 grid-rows-6 gap-3 w-4/5 mx-auto font-cairo mt-20">
+      
+      <div className="relative flex justify-center mt-20 ">
+<div className="row-span-2 border hover:border-segundo hover:animate-lightup w-[450px] ml-10 mb-10">
+        <h5 className="flex items-center justify-between uppercase font-bold bg-segundo text-white px-2 py-2">
+        PERFIL <i className="fa-solid fa-circle-plus text-white"></i>
+        </h5>
+        <div className="grid grid-cols-2 w-1/4">
+        <div>
+        {user.image ? (
+        <img src={user.image} alt="User Avatar" className="text-9xl text-gray-200 mt-2" />
+      ) : (
+        <i className="fa-solid fa-image-portrait text-9xl text-gray-200 mt-2"></i>
+      )}
+    </div>
+    <div className="w-72 ml-14 cursor-pointer h-40" onClick={handleNavigateToProfile}>
+      <p>Nombre y Apellido: {user.name} {user.surname}</p>
+      <p>E-mail: {user.email}</p>
+      <p>Ubicación: {user.location}</p>
+      <p>Teléfono: {user.number}</p>
+      <p>Fecha de Nacimiento: {user.Date}</p>
+      <p>Género: {user.gender}</p>
+    </div>
+  </div>
+</div>
+
+<div className="row-span-2 border hover:border-primero hover:animate-lightup w-[450px] ml-10 mb-10" onClick={handleNavigateToVacaciones}>
+        <h5 className="flex items-center justify-between uppercase font-bold bg-primero text-white px-2 py-2">
+        MIS VIAJES <i className="fa-solid fa-circle-plus text-white"></i>
+        </h5>
+        <div className="h-40">
+        {user.Rents && user.Rents.map((rent:any, index: number) => (
+              <p key={rent.id}>{`${index+1}) Me voy de vacaciones desde el ${rent.start_date} hasta el ${rent.end_date}`}</p>
+            ))}
+            </div>
+
+      </div>
+
+      <div className="row-span-2 border hover:border-tercero hover:animate-lightup w-[450px] ml-10 mb-10">
+        <h5 className="flex items-center justify-between uppercase font-bold bg-tercero text-white px-2 py-2">
+        MIS FAVORITOS <i className="fa-solid fa-circle-plus text-white"></i>
+        </h5>
+        <div className="h-40">
+        {user.Rents && user.Rents.map((rent:any, index: number) => (
+              <p key={rent.id}>{`${index+1}) Me voy de vacaciones desde el ${rent.start_date} hasta el ${rent.end_date}`}</p>
+            ))}
+            </div>
+      </div>
+      </div>
+
+
+      <div className="relative flex justify-center">
+        
+<div className="row-span-2 border hover:border-tercero hover:animate-lightup w-[450px] ml-10 mb-10" onClick={handleNavigateToAnuncio}>
+        <h5 className="flex items-center justify-between uppercase font-bold bg-tercero text-white px-2 py-2">
+        MIS ANUNCIOS <i className="fa-solid fa-circle-plus text-white"></i>
+        </h5>
+        <div className="ml-1">
+        {userProperties && userProperties.map((properties:any, index:number) => (
+          <p key={properties.id}>{`${index+1}) ${properties.title}`}</p>
+        ))}
+  </div>
+</div>
+
+<div className="row-span-2 border hover:border-primero hover:animate-lightup w-[450px] ml-10 mb-10" onClick={handleNavigateToReservas}>
+        <h5 className="flex items-center justify-between uppercase font-bold bg-primero text-white px-2 py-2">
+        RESERVARON MI PROPIEDAD <i className="fa-solid fa-circle-plus text-white"></i>
+        </h5>
+        <div className="h-40">
+        {userProperties && userProperties.map((property: any) => (
+    property.Rents && property.Rents.map((rent: any, index: number) => (
+      <p key={rent.id}>{`${index+1}) Reservada desde el ${rent.start_date} hasta el ${rent.end_date}`}</p>
+    ))
+  ))}
+            </div>
+
+      </div>
+
+      <div className="row-span-2 border hover:border-segundo hover:animate-lightup w-[450px] ml-10 mb-10">
+        <h5 className="flex items-center justify-between uppercase font-bold bg-segundo text-white px-2 py-2">
+        PAGOS Y COBROS <i className="fa-solid fa-circle-plus text-white"></i>
+        </h5>
+        <div className="h-40">
+        {user.Rents && user.Rents.map((rent:any, index: number) => (
+              <p key={rent.id}>{`${index+1}) Me voy de vacaciones desde el ${rent.start_date} hasta el ${rent.end_date}`}</p>
+            ))}
+            </div>
+      </div>
+      </div>
+</div>
+  );
+};
+
+export default Account;
+
+{/* <div className="grid grid-cols-3 grid-rows-6 gap-3 w-4/5 mx-auto font-cairo mt-20">
         <div className="row-span-2 rounded-xl p-2 border hover:border-argentina hover:animate-lightup">
           <div className="grid grid-cols-2 w-1/4">
             <div>
@@ -84,10 +178,4 @@ const Account = () => {
           <h1 className="uppercase font-bold">mensajes</h1>
           <h3>(no tenes mensajes nuevos)</h3>
         </div>
-      </div>
-    </div>
-  );
-};
-
-export default Account;
-
+      </div> */}
