bfc263a810bfba64d5853ffe43029f0e6dfc53d5
MercedesPoisson <mercedespoisson@yahoo.com>
2023-07-02 13:39:51 -0300

    subtitulos on

--- Client/src/views/Team/Team.tsx
+++ Client/src/views/Team/Team.tsx
@@ -1,6 +1,7 @@
 // import UserBar from "../../components/searchBar/UserBar";
 // import { useNavigate } from "react-router-dom";
 import Perfil from "./perfil.png";
+import Perfil1 from "./Perfil1.jpeg";
 
 const Team = () => {
     // const navigate = useNavigate();
@@ -10,26 +11,7 @@ const Team = () => {
     
     return (
         <div>
-            {/* <div className="sticky top-0">
-        <div className="grid grid-cols-2 gap-3 h-16 mb-1 bg-white">
-          <div className="col-span-1 flex items-center justify-start">
-            <div
-              className="flex items-center cursor-pointer"
-              onClick={handleNavigateToHome}
-            >
-              <i className="fa fa-sun text-argentina ml-4 text-2xl"></i>
-              <span className="ml-1 text-argentina font-comfortaa text-lg">
-                argentina
-              </span>
-            </div>
-          </div>
-          <div className="col-span-1 font-cairo-play flex items-center justify-end mr-10">
-            <button className="mr-4">
-            <UserBar />
-            </button>
-          </div>
-        </div>
-      </div> */}
+            
       <div className="font-Poppins bg-dark_purple text-white items-center h-screen ">
         <div className="text-center py-10">
             <h5 className="text-4xl w-96 mx-auto leading-normal font-bold mb-12">El Equipo</h5>
@@ -49,7 +31,7 @@ const Team = () => {
                             </a>
                     </div>
                     
-                    <button className="bg-btn_primary py-2.5 px-8 rounded-full">Contactar</button>
+                    <button className="bg-btn_primary py-2.5 px-8 rounded-full"><a href="mailto:mercedespoisson@yahoo.com">Contactar</a></button>
                 </div>
             
                 <div className="bg-white/10 duration-500 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 p-8 rounded-xl mix-blend-luminosity">
@@ -61,19 +43,19 @@ const Team = () => {
                        <i className="fa-brands fa-github text-btn_primary text-2xl mr-4"></i>
                         <i className="fa-brands fa-linkedin text-btn_primary text-2xl"></i> 
                     </div>
-                    <button className="bg-btn_primary py-2.5 px-8 rounded-full">Contactar</button>
+                    <button className="bg-btn_primary py-2.5 px-8 rounded-full"><a href="mailto:mercedespoisson@yahoo.com">Contactar</a></button>
                 </div>
             
                 <div className="bg-white/10 duration-500 p-8 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 rounded-xl mix-blend-luminosity">
-                <img src={Perfil} className="h-20 mx-auto rounded-full" />
+                <img src={Perfil1} className="h-20 mx-auto rounded-full" />
                 <h4 className="uppercase text-xl font-bold">Francisco Galliani</h4>
                 <p className="text-sm leading-7 my-3 font-light opacity-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Eius earum sequi omnis tempore nesciunt odio!</p>
                     <div className="mt-10">
-                       <i className="fa-brands fa-github text-btn_primary text-2xl mr-4"></i>
-                        <i className="fa-brands fa-linkedin text-btn_primary text-2xl"></i> 
+                       <a href="https://github.com/franciscogalliani" target="blank" rel="noopener noreferrer"><i className="fa-brands fa-github text-btn_primary text-2xl mr-4"></i></a>
+                        <a href="https://www.linkedin.com/in/francisco-galliani-54b0b9206/" target="blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin text-btn_primary text-2xl"></i></a> 
                     </div>
-                    <button className="bg-btn_primary py-2.5 px-8 rounded-full">Contactar</button>
+                    <button className="bg-btn_primary py-2.5 px-8 rounded-full"><a href="mailto:fjgalliani@hotmail.com">Contactar</a></button>
                 </div>
             
                 <div className="bg-white/10 duration-500 p-8 group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 rounded-xl mix-blend-luminosity">
