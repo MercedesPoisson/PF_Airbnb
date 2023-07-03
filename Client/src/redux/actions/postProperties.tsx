94895c9bffd881f2ea28421d81373f795a1f72ac
MercedesPoisson <mercedespoisson@yahoo.com>
2023-07-03 11:34:24 -0300

    links locales

--- Client/src/redux/actions/postProperties.tsx
+++ Client/src/redux/actions/postProperties.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const postServices = (formData: FormData) => {
     return async (dispatch: Dispatch<AnyAction>) => {
-        await axios.post("https://pfback-production-a519.up.railway.app/property/", formData, {
+        await axios.post("http://localhost:3001/property/", formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
